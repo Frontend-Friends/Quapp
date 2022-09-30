6e7e960220813fe4b7449cf09b5614d69834ac58
kerbage <lkerbage@gmx.net>
2022-09-30 18:10:51 +0200

    Formatting and React.FC instead of plain function

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,78 +1,74 @@
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
-// import { useAuth } from '../context/AuthContext'
-import Button from '@mui/material/Button';
-import {FormGroup, Input} from "@mui/material";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { FormGroup, Input } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
 
+const Login: React.FC = () => {
+  const router = useRouter();
+  //@ts-ignore
+  const { user, login } = useAuth();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
 
-const Login = () => {
-    const router = useRouter()
-    // const { user, login } = useAuth()
-    const [data, setData] = useState({
-        email: '',
-        password: '',
-    })
-
-    const handleLogin = async (e: any) => {
-        e.preventDefault()
-
-        // console.log(user)
-        try {
-            console.log(data);
-            // await login(data.email, data.password)
-            router.push('/Dashboard')
-        } catch (err) {
-            console.log(err)
-        }
+    try {
+      await login(data.email, data.password);
+      console.log("user is", user);
+      router.push("/dashboard");
+    } catch (err) {
+      console.error("error is", err);
     }
+  };
 
-    return (
-        <div
-            style={{
-                width: '40%',
-                margin: 'auto',
-            }}
-        >
-            <h1 className="text-center my-3 ">Login</h1>
-            <form onSubmit={handleLogin}>
-                <FormGroup className="mb-3">
-                    <label>Email address</label>
-                    <Input
-                        onChange={(e: any) =>
-                            setData({
-                                ...data,
-                                email: e.target.value,
-                            })
-                        }
-                        value={data.email}
-                        required
-                        type="email"
-                        placeholder="Enter email"
-                    />
-                </FormGroup>
+  return (
+    <div
+      style={{
+        width: "40%",
+        margin: "auto",
+      }}
+    >
+      <h1 className="text-center my-3 ">Login</h1>
+      <form onSubmit={handleLogin}>
+        <FormGroup className="mb-3">
+          <label>Email address</label>
+          <Input
+            onChange={(e: any) =>
+              setData({
+                ...data,
+                email: e.target.value,
+              })
+            }
+            value={data.email}
+            required
+            type="email"
+            placeholder="Enter email"
+          />
+        </FormGroup>
 
-                <FormGroup className="mb-3" >
-                    <label>Password</label>
-                    <Input
-                        onChange={(e: any) =>
-                            setData({
-                                ...data,
-                                password: e.target.value,
-                            })
-                        }
-                        value={data.password}
-                        required
-                        type="password"
-                        placeholder="Password"
-                    />
-                </FormGroup>
-                <Button  type="submit">
-                    Login
-                </Button>
-            </form>
-        </div>
-    )
-}
+        <FormGroup className="mb-3">
+          <label>Password</label>
+          <Input
+            onChange={(e: any) =>
+              setData({
+                ...data,
+                password: e.target.value,
+              })
+            }
+            value={data.password}
+            required
+            type="password"
+            placeholder="Password"
+          />
+        </FormGroup>
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
