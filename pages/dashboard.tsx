a84897fb91d8deefecd92e43a13a632d0c182bb6
kerbage <lkerbage@gmx.net>
2022-09-30 18:11:22 +0200

    Formatting and React.FC instead of plain function

---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      This route is protected. You can reach it by applying correct credentials.
+    </div>
+  );
+};
+
+export default Dashboard;
