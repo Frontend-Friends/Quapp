731831664b73d264d201695e232a58c5bedc7895
kerbage <lkerbage@gmx.net>
2022-09-30 18:09:49 +0200

    changed div to button and formatting

--- src/layout/Components/Nav.tsx
+++ src/layout/Components/Nav.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import {Container} from "@mui/material";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Container } from "@mui/material";
+import { useAuth } from "../../../context/AuthContext";
 
 const Nav = () => {
-    // const { user, logout } = useAuth()
-    const user = ""
-    const logout = () =>  {}
-    const router = useRouter()
+  //  @ts-ignore
+  const { user, logout } = useAuth();
 
-    return (
-            <Container>
+  const router = useRouter();
 
+  return (
+    <Container>
+      <div className="me-auto">
+        {user ? (
+          <div>
+            <button
+              onClick={() => {
+                logout();
+                router.push("/Login").then();
+              }}
+            >
+              <h2>You are logged in</h2>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <>
+            <Link href="/Signup" passHref>
+              <div>Signup</div>
+            </Link>
+            <Link href="/Login" passHref>
+              <div>Login</div>
+            </Link>
+          </>
+        )}
+      </div>
+    </Container>
+  );
+};
 
-                    <div className="me-auto">
-                        {user ? (
-                            <div>
-                                <div
-                                    onClick={() => {
-                                        logout()
-                                        router.push('/login')
-                                    }}
-                                >
-                                    Logout
-                                </div>
-                            </div>
-                        ) : (
-                            <>
-                                <Link href="/Signup" passHref>
-                                    <div>Signup</div>
-                                </Link>
-                                <Link href="/Login" passHref>
-                                    <div>Login</div>
-                                </Link>
-                            </>
-                        )}
-                    </div>
-            </Container>
-    )
-}
-
-export default Nav
+export default Nav;
