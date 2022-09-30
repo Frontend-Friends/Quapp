f084426fda58dd516222f37cc5bf1e06f6950c2a
kerbage <lkerbage@gmx.net>
2022-09-30 18:08:29 +0200

    Made functional component (React.FC) and added functionality to login

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -1,74 +1,72 @@
-import React, { useState } from 'react'
-import { useAuth } from '../context/AuthContext'
-import {Button, FormGroup, Input} from "@mui/material";
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Button, FormGroup, Input } from "@mui/material";
 
-const Signup = () => {
-    // @ts-ignore
-    const { user, signup } = useAuth()
-    console.log(user)
-    const [data, setData] = useState({
-        email: '',
-        password: '',
-    })
+const Signup: React.FC = () => {
+  // @ts-ignore
+  const { user, signup } = useAuth();
+  console.log(user);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleSignup = async (e: any) => {
-        e.preventDefault()
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
 
-        try {
-             await signup(data.email, data.password)
-        } catch (err) {
-            console.log(err)
-        }
-        console.log(data)
+    try {
+      await signup(data.email, data.password);
+    } catch (err) {
+      console.log(err);
     }
+    console.log(data);
+  };
 
-    return (
-        <div
-            style={{
-                width: '40%',
-                margin: 'auto',
-            }}
-        >
-            <h1 className="text-center my-3 ">Signup</h1>
-            <form onSubmit={handleSignup}>
-                <FormGroup className="mb-3">
-                    <label>Email address</label>
-                    <Input
-                        type="email"
-                        placeholder="Enter email"
-                        required
-                        onChange={(e: any) =>
-                            setData({
-                                ...data,
-                                email: e.target.value,
-                            })
-                        }
-                        value={data.email}
-                    />
-                </FormGroup>
+  return (
+    <div
+      style={{
+        width: "40%",
+        margin: "auto",
+      }}
+    >
+      <h1 className="text-center my-3 ">Signup</h1>
+      <form onSubmit={handleSignup}>
+        <FormGroup className="mb-3">
+          <label>Email address</label>
+          <Input
+            type="email"
+            placeholder="Enter email"
+            required
+            onChange={(e: any) =>
+              setData({
+                ...data,
+                email: e.target.value,
+              })
+            }
+            value={data.email}
+          />
+        </FormGroup>
 
-                <FormGroup className="mb-3">
-                    <label>Password</label>
-                    <Input
-                        type="password"
-                        placeholder="Password"
-                        required
-                        onChange={(e: any) =>
-                            setData({
-                                ...data,
-                                password: e.target.value,
-                            })
-                        }
-                        value={data.password}
-                    />
-                </FormGroup>
+        <FormGroup className="mb-3">
+          <label>Password</label>
+          <Input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={(e: any) =>
+              setData({
+                ...data,
+                password: e.target.value,
+              })
+            }
+            value={data.password}
+          />
+        </FormGroup>
 
-                <Button  type="submit">
-                    Signup
-                </Button>
-            </form>
-        </div>
-    )
-}
+        <Button type="submit">Signup</Button>
+      </form>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
