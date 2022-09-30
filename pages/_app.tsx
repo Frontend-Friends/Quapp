cb3665a5bc78179e694effe6d91cd3b0bc55e67a
kerbage <lkerbage@gmx.net>
2022-09-30 18:12:54 +0200

    Formatting and added ProtectedRoute and paths without protected route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-
-import * as React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../config/theme';
-import createEmotionCache from '../config/createEmotionCache';
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
 import Nav from "../src/layout/Components/Nav";
-import {AuthContextProvider} from "../context/AuthContext";
-
+import { AuthContextProvider } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import ProtectedRoute from "../src/components/auth/ProtectedRoute";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -18,23 +18,33 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-export default function App (props: MyAppProps) {
+export default function App(props: MyAppProps) {
+  const router = useRouter();
+  const noAuthRequired = ["/", "/login", "/signup"];
+
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Quapp - sharing is caring</title>
+      </Head>
 
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width"  />
-<title>Quapp - sharing is caring</title>
-        </Head>
-
-        <ThemeProvider theme={theme}>
-            <AuthContextProvider>
+      <ThemeProvider theme={theme}>
+        <AuthContextProvider>
           <CssBaseline />
-            <Nav/>
-          <Component {...pageProps} />
-            </AuthContextProvider>
-        </ThemeProvider>
-      </CacheProvider>
+          <Nav />
+          {noAuthRequired.includes(router.pathname) ? (
+            <Component {...pageProps} />
+          ) : (
+            // @ts-ignore
+            <ProtectedRoute>
+              <Component {...pageProps} />
+            </ProtectedRoute>
+          )}
+        </AuthContextProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
