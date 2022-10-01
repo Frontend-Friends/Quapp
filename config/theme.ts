ce068ea6e8f08f7a5c94d8ce5a003882a8e6b7e1
Philip Park <philipp@i-web.ch>
2022-10-01 17:20:11 +0200

    fix: slight layout adaption in login feat: added spacing in theme

--- config/theme.ts
+++ config/theme.ts
@@ -3,6 +3,7 @@ import { createTheme } from '@mui/material/styles'
 // Create a theme instance.
 // Theme tree structure: https://mui.com/material-ui/customization/default-theme/?expand-path=$.palette
 const theme = createTheme({
+  spacing: 8,
   palette: {
     primary: {
       main: '#0072ff',
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { FC, FormEventHandler, useState } from 'react'
 import Button from '@mui/material/Button'
-import { FormGroup, Input } from '@mui/material'
+import { FormGroup, Input, TextField } from '@mui/material'
 import { useAuth } from '../components/context/auth-context'
 
 const Login: FC = () => {
@@ -19,7 +19,7 @@ const Login: FC = () => {
     try {
       await login(data.email, data.password)
       console.log('user is', user)
-      router.push('/dashboard')
+      await router.push('/dashboard')
     } catch (err) {
       console.error('error is', err)
     }
@@ -32,11 +32,10 @@ const Login: FC = () => {
         margin: 'auto',
       }}
     >
-      <h1 className="text-center my-3 ">Login</h1>
+      <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <FormGroup className="mb-3">
-          <label>Email address</label>
-          <Input
+        <FormGroup sx={{ my: 2 }}>
+          <TextField
             onChange={(e) =>
               setData({
                 ...data,
@@ -46,13 +45,13 @@ const Login: FC = () => {
             value={data.email}
             required
             type="email"
-            placeholder="Enter email"
+            label="E-Mail"
+            variant="outlined"
           />
         </FormGroup>
 
-        <FormGroup className="mb-3">
-          <label>Password</label>
-          <Input
+        <FormGroup sx={{ my: 2 }}>
+          <TextField
             onChange={(e) =>
               setData({
                 ...data,
@@ -62,10 +61,13 @@ const Login: FC = () => {
             value={data.password}
             required
             type="password"
-            placeholder="Password"
+            label="Password"
+            variant="outlined"
           />
         </FormGroup>
-        <Button type="submit">Login</Button>
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
       </form>
     </div>
   )
