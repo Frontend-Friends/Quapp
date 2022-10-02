d9e5b621b6d4b8a4d15619635827730209b36372
kerbage <lkerbage@gmx.net>
2022-10-02 10:39:56 +0200

    auth

--- pages/unauthorized.tsx
+++ pages/unauthorized.tsx
@@ -3,11 +3,20 @@ import React, { FormEvent } from 'react'
 const Unauthorized: React.FC = () => {
   const handleSignup = async (e: FormEvent) => {
     e.preventDefault()
-    const data = await fetch('/api/signup')
-      .then((response) => response.json())
-      .then((json) => console.log(json))
+    const email: string = e.target[0].value
+    const password: string = e.target[1].value
+
+    const data = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        accept: 'application.json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+      cache: 'default',
+    })
   }
-  const handleLogin = async (e: FormEvent) => {
+  const handleLogin = async (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
     const email: string = e.target[0].value
     const password: string = e.target[1].value
@@ -29,20 +38,26 @@ const Unauthorized: React.FC = () => {
       .then((response) => response.json())
       .then((json) => console.log(json))
   }
+
   return (
     <>
       Unauthorized content
       <form onSubmit={handleLogin}>
         <br />
-        <input name="email" value="lkerbage@gmx.net" />{' '}
-        <input name="password" value="123456" />
-        <button onClick={handleSignup}>Signup</button>
-        <br />
+        <input name="email" defaultValue="lkerbage@gmx.net" />{' '}
+        <input name="password" defaultValue="123456" />
         <button type="submit">login</button>
         <br />
-        <button onClick={handleLogout}>Logout</button>
-        <br />
       </form>
+      <br />
+      <form onSubmit={handleSignup}>
+        <br />
+        <input name="email" defaultValue="lkerbage@gmx.net" />{' '}
+        <input name="password" defaultValue="123456" />
+        <button type="submit">Signup</button>
+      </form>
+      <br />
+      <button onClick={handleLogout}>Logout</button>
     </>
   )
 }
