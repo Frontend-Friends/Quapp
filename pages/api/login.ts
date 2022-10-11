09eb2e2b44dd3db68ec7e0ec28e72f773e5ecd23
kerbage <lkerbage@gmx.net>
2022-10-11 20:17:49 +0200

    auth

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -40,18 +40,3 @@ async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
 }
 
 export default withIronSessionApiRoute(loginRoute, ironOptions)
-
-// const { username } = await req.body;
-//
-// try {
-//   const {
-//     data: { login, avatar_url },
-//   } = await octokit.rest.users.getByUsername({ username });
-//
-//   const user = { isLoggedIn: true, login, avatarUrl: avatar_url } as User;
-//   req.session.user = user;
-//   await req.session.save();
-//   res.json(user);
-// } catch (error) {
-//   res.status(500).json({ message: (error as Error).message });
-// }
