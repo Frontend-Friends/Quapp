dd2c025555d946040b79c4318971f5c33205f915
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2022-10-12 22:36:49 +0200

    Add files

--- components/condensed-container.tsx
+++ components/condensed-container.tsx
@@ -1,19 +1,20 @@
-import { FC, ReactNode } from 'react'
-import { Box } from '@mui/material'
+import { forwardRef } from 'react'
+import { Box, BoxProps } from '@mui/material'
 
-export const CondensedContainer: FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  return (
-    <Box
-      sx={{
-        width: { xs: '90%', md: '50%', lg: '40%' },
-        maxWidth: '600px',
-        mx: 'auto',
-        my: '10%',
-      }}
-    >
-      {children}
-    </Box>
-  )
-}
+export const CondensedContainer = forwardRef<HTMLDivElement, BoxProps>(
+  (props, ref) => {
+    return (
+      <Box
+        {...props}
+        sx={{
+          width: '100%',
+          maxWidth: '600px',
+          mx: 'auto',
+          my: '10%',
+          ...props.sx,
+        }}
+        ref={ref}
+      />
+    )
+  }
+)
---
+++ components/products/types.ts
@@ -0,0 +1,26 @@
+export type ChatMessage = {
+  dateTime: string
+  fromOwner: boolean
+  message: string
+}
+
+export type ProductChatType = {
+  chatUserId: string
+  chatUserName: string | null
+  history: ChatMessage[]
+}
+
+export type ProductType = {
+  id: string
+  title: string
+  lead?: string
+  text: string
+  description?: string
+  imgSrc?: string
+  isAvailable: boolean
+  owner: {
+    id: string
+    userName: string
+  }
+  chats: ProductChatType[]
+}
