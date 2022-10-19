bdbc0b803c8ba005c7029cbe69f9fd90ac767e7f
Philip Park <shixxor@gmail.com>
2022-10-19 15:13:00 +0200

    feat: spaces translations added

--- mock/translations/translations.ts
+++ mock/translations/translations.ts
@@ -19,4 +19,9 @@ export const translations: Record<string, string> = {
   PRODUCTS_title: 'Produkte',
   PRODUCTS_no_entries: 'Leider gibt es noch keine Einträge.',
   PRODUCT_not_available: 'Nicht verfügbar',
+
+  SPACES_title: 'Deine Spaces',
+  SPACES_no_entries: 'Es gibt noch keine Spaces.',
+  SPACES_members: 'Mitglieder',
+  SPACES_items: 'Einträge',
 }
