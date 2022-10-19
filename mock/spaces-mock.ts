8a52de9820316f8deba6e7157d7289d870bb0dc1
Philip Park <shixxor@gmail.com>
2022-10-19 15:12:49 +0200

    feat: spaces mock data added

---
+++ mock/spaces-mock.ts
@@ -0,0 +1,29 @@
+export const SPACES_MOCK = [
+  {
+    name: 'Wetziker Hood',
+    id: '01',
+    ownerId: 'ahCptbVoyyNljQaAX9x27uyHISg1',
+    creatorId: 'ahCptbVoyyNljQaAX9x27uyHISg1',
+    creationDate: '2022-10-01T22:15:43.511Z',
+    memberCount: '12',
+    itemCount: '23',
+  },
+  {
+    name: 'Quartierverein Züri Kreis 4',
+    id: '02',
+    creatorId: 'ahCptbVoyyNljQaAX9x27uyHISg1',
+    ownerId: 'POs0wUxWedQKaZfljA6hH6nVmaP2',
+    creationDate: '2022-10-01T08:29:43.511Z',
+    memberCount: '862',
+    itemCount: '8104',
+  },
+  {
+    name: 'WWWW W WWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWW',
+    id: '03',
+    ownerId: '',
+    creatorId: 'ahCptbVoyyNljQaAX9x27uyHISg1',
+    creationDate: '2021-06-02T21:55:43.511Z',
+    memberCount: '8888',
+    itemCount: '888888',
+  },
+]
