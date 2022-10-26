b88a5312b07d07381fe5fa076389baec6e4b4ef7
Philip Park <not@your.concern>
2022-10-26 22:06:28 +0200

    fix: react hydration problem fixed by removing overlapping CardActionArea and using next link and mui link instead feat: added accessible titles feat: trivial stylings added feat: translations added

--- components/spaces/space-item.tsx
+++ components/spaces/space-item.tsx
@@ -1,9 +1,9 @@
 import {
   Card,
-  CardActionArea,
   CardContent,
   CardHeader,
   Grid,
+  Link as MuiLink,
   IconButton,
   Typography,
 } from '@mui/material'
@@ -15,6 +15,8 @@ import MoreVertIcon from '@mui/icons-material/MoreVert'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import React from 'react'
+import { useTranslation } from '../../hooks/use-translation'
+import Link from 'next/link'
 
 interface Props {
   space: SpaceItemType
@@ -30,70 +32,86 @@ const SpaceItem: FC<Props> = ({ space }) => {
     setAnchorEl(null)
   }
 
+  const t = useTranslation()
+
   return (
-    <Grid item xs={2} sx={{ flexGrow: '1' }}>
-      <Card variant="outlined">
-        <CardActionArea disableRipple>
-          <CardHeader
-            title={space.name}
-            action={
-              <IconButton
-                aria-label="settings"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-              >
-                <MoreVertIcon />
-              </IconButton>
-            }
+    <Grid item xs={1} sx={{ flexGrow: '1' }}>
+      <Card variant="outlined" sx={{ position: 'relative' }}>
+        <Link href={'#'} passHref>
+          <MuiLink
+            title={`${t('GLOBAL_open')} ${space.name}`}
+            style={{
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              display: 'block',
+            }}
           />
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <MenuItem onClick={handleClose}>Bearbeiten</MenuItem>
-            <MenuItem onClick={handleClose}>Löschen</MenuItem>
-          </Menu>
-          <CardContent
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            {space.memberCount && (
-              <Typography
-                align="justify"
-                variant="body2"
-                sx={{
-                  mr: 3,
-                  display: 'flex',
-                  alignItems: 'center',
-                }}
-              >
-                <GroupsIcon sx={{ mr: 1 }} /> {space.memberCount}
-              </Typography>
-            )}
-            {space.itemCount && (
-              <Typography
-                align="justify"
-                variant="body2"
-                sx={{
-                  mr: 3,
-                  display: 'flex',
-                  alignItems: 'center',
-                }}
-              >
-                <CategoryIcon sx={{ mr: 1 }} /> {space.itemCount}
-              </Typography>
-            )}
-          </CardContent>
-        </CardActionArea>
+        </Link>
+        <CardHeader
+          title={space.name}
+          action={
+            <IconButton
+              title={`${space.name} ${t('GLOBAL_options')}`}
+              aria-label="settings"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{ position: 'relative' }}
+            >
+              <MoreVertIcon />
+            </IconButton>
+          }
+        />
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>{t('GLOBAL_edit')}</MenuItem>
+          <MenuItem onClick={handleClose}>{t('GLOBAL_delete')}</MenuItem>
+        </Menu>
+        <CardContent
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          {space.memberCount && (
+            <Typography
+              align="justify"
+              variant="body2"
+              sx={{
+                mr: 3,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <GroupsIcon sx={{ mr: 1 }} />
+              {space.memberCount}
+            </Typography>
+          )}
+          {space.itemCount && (
+            <Typography
+              align="justify"
+              variant="body2"
+              sx={{
+                mr: 3,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <CategoryIcon sx={{ mr: 1 }} /> {space.itemCount}
+            </Typography>
+          )}
+        </CardContent>
       </Card>
     </Grid>
   )
