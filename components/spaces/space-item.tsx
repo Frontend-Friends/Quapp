c2244e1ac1cb26b177eb01912cb408bfd168d417
Philip Park <not@your.concern>
2022-10-26 20:03:01 +0200

    feat: added icons and options buttons with function space component

--- components/spaces/space-item.tsx
+++ components/spaces/space-item.tsx
@@ -1,38 +1,99 @@
 import {
   Card,
-  CardActions,
+  CardActionArea,
   CardContent,
   CardHeader,
   Grid,
+  IconButton,
   Typography,
 } from '@mui/material'
 import { FC } from 'react'
-import { useTranslation } from '../../hooks/use-translation'
 import { SpaceItemType } from '../products/types'
+import GroupsIcon from '@mui/icons-material/Groups'
+import CategoryIcon from '@mui/icons-material/Category'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import React from 'react'
 
 interface Props {
   space: SpaceItemType
 }
 
 const SpaceItem: FC<Props> = ({ space }) => {
-  const t = useTranslation()
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Grid item xs={1} sx={{ flexGrow: '1' }}>
+    <Grid item xs={2} sx={{ flexGrow: '1' }}>
       <Card variant="outlined">
-        <CardHeader title={space.name} />
-        <CardContent>
-          {space.memberCount && (
-            <Typography variant="body2">
-              {t('SPACES_members')}: {space.memberCount}
-            </Typography>
-          )}
-          {space.itemCount && (
-            <Typography variant="body2">
-              {t('SPACES_items')}: {space.itemCount}
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions>Settings</CardActions>
+        <CardActionArea disableRipple>
+          <CardHeader
+            title={space.name}
+            action={
+              <IconButton
+                aria-label="settings"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            }
+          />
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}>Bearbeiten</MenuItem>
+            <MenuItem onClick={handleClose}>Löschen</MenuItem>
+          </Menu>
+          <CardContent
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            {space.memberCount && (
+              <Typography
+                align="justify"
+                variant="body2"
+                sx={{
+                  mr: 3,
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <GroupsIcon sx={{ mr: 1 }} /> {space.memberCount}
+              </Typography>
+            )}
+            {space.itemCount && (
+              <Typography
+                align="justify"
+                variant="body2"
+                sx={{
+                  mr: 3,
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <CategoryIcon sx={{ mr: 1 }} /> {space.itemCount}
+              </Typography>
+            )}
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
   )
