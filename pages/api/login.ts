760421f8a43bc444a14e4aa5e44979e2675edd5b
kerbage <lkerbage@gmx.net>
2022-10-29 15:24:36 +0200

    fetch data to get user from db

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,8 +1,9 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { ironOptions } from '../../lib/config'
+import { ironOptions, server } from '../../lib/config'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../config/firebase'
+import { fetchJson } from '../../lib/helpers/fetch-json'
 
 declare module 'iron-session' {
   interface IronSessionData {
@@ -17,6 +18,10 @@ const login = (email: string, password: string) => {
   return signInWithEmailAndPassword(auth, email, password)
 }
 
+const getUser = async (email: string) => {
+  await fetchJson(`${server}/api/user-db?email=${email}`)
+}
+
 async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
   try {
     const email = req.body.email
@@ -28,7 +33,7 @@ async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
       email,
       uid: credentials.user.uid,
     })
-    //todo @lk set user to store
+    await getUser(email)
 
     await req.session.save()
     res.send({ session: true })
