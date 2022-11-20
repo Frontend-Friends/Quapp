e143e1a7baf5ad6cf9260a7996e325a09bb1b3b7
kerbage <lkerbage@gmx.net>
2022-11-20 18:05:15 +0100

    using exported ironOptions instead of typing cookieName, pw, etc. every time when using them.

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,8 @@
 import { IronSessionOptions } from 'iron-session'
+import { sessionOptions } from '../config/session-config'
 
 export const ironOptions: IronSessionOptions = {
-  cookieName: '__session',
-  password: process.env.SECRET_COOKIE_PASSWORD as string,
-  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
+  ...sessionOptions,
 }
 
 const dev = process.env.NODE_ENV !== 'production'
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,16 +1,12 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { sessionOptions } from '../../config/session-config'
 
 export default withIronSessionApiRoute(
   function userRoute(req: NextApiRequest, res: NextApiResponse) {
     res.send({ user: req.session.user })
   },
   {
-    cookieName: '__session',
-    password: process.env.SECRET_COOKIE_PASSWORD as string,
-    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-    cookieOptions: {
-      secure: process.env.NODE_ENV === 'production',
-    },
+    ...sessionOptions,
   }
 )
