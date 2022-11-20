940c239532cc9a74b1a326af3a805db48f91117a
kerbage <lkerbage@gmx.net>
2022-11-20 18:19:15 +0100

    Outcomment buttons in nav

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -59,20 +59,19 @@ export const NavBar: FC<{ linkList: LinkProps[]; isLoggedIn: boolean }> = ({
           </Typography>
 
           {location.pathname !== '/login' &&
-            // (isLoggedOut ? (
-            //   <Button color="secondary" variant="contained">
-            //     {t('LOGIN_login')}
-            //   </Button>
-            // ) : (
-            //   <Button
-            //     color="secondary"
-            //     variant="contained"
-            //     onClick={handleLogout}
-            //   >
-            //     {t('LOGOUT_logout')}
-            //   </Button>
-            // )
-            null}
+            (isLoggedOut ? (
+              <Button color="secondary" variant="contained">
+                {t('LOGIN_login')}
+              </Button>
+            ) : (
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={handleLogout}
+              >
+                {t('LOGOUT_logout')}
+              </Button>
+            ))}
         </Toolbar>
       </AppBar>
       <NavigationDrawer
