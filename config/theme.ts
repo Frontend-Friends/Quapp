c3097631ca1d140cad9d8f75be7de8794b834e17
Philip Park <not@your.concern>
2022-12-04 18:23:02 +0100

    feat: added text contrast colors to theme primary and secondary

--- config/theme.ts
+++ config/theme.ts
@@ -10,11 +10,13 @@ const theme = createTheme({
       main: twTheme.extend.colors.violetRed[600],
       light: twTheme.extend.colors.violetRed[300],
       dark: twTheme.extend.colors.violetRed[900],
+      contrastText: twTheme.extend.colors.violetRed.contrastText,
     },
     secondary: {
       main: twTheme.extend.colors.mintGreen[600],
       light: twTheme.extend.colors.mintGreen[300],
       dark: twTheme.extend.colors.mintGreen[900],
+      contrastText: twTheme.extend.colors.mintGreen.contrastText,
     },
     error: {
       main: '#d53030',
