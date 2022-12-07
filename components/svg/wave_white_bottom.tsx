27910a60f709be93da3ccdbfb1c53a1f60811d3c
Philip Park <not@your.concern>
2022-12-07 16:17:50 +0100

    feat: added a new SVGs

---
+++ components/svg/wave_white_bottom.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from 'react'
+
+export const WaveWhiteBottomSVG = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 154.53" {...props}>
+      <g>
+        <path
+          fill="#FFFFFF"
+          d="M.56,126.08c-.18-.46-.37-.94-.56-1.43v29.87H375V0C359.09,121.63,26.54,190.26,.56,126.08Z"
+        />
+      </g>
+    </svg>
+  )
+}
---
+++ components/svg/wave_white_top.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from 'react'
+
+export const WaveWhiteTopSVG = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 63.52" {...props}>
+      <g>
+        <path
+          fill="#FFFFFF"
+          d="M.56,47.34C56.94-36.33,211.15,18.38,375,63.52V0H0V48.2c.19-.29,.37-.58,.56-.86Z"
+        />
+      </g>
+    </svg>
+  )
+}
