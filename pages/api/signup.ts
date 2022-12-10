bfeab5327c6965a1c6a367f45b958ae057ad9946
kerbage <lkerbage@gmx.net>
2022-12-10 16:20:14 +0100

    Send e-mail to firebase for confirmation in the get request of signup

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -12,9 +12,8 @@ export default async function signupRoute(
 ) {
   // todo export fn:
   const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: `http://localhost:3000/auth/email-confirmation`,
+    url: `http://localhost:3000/auth/email-confirmation?email=${req.body.email}`,
     // This must be true.
     handleCodeInApp: true,
   }
