badd66f93477bb28fbc49ee5ba38dd6ad66884f5
Philip Park <not@your.concern>
2022-12-11 15:07:01 +0100

    feat: added new font weight to "Jost"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,7 @@ class MyDocument extends Document<{ emotionStyleTags: ReactNode[] }> {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
           {this.props.emotionStyleTags}
