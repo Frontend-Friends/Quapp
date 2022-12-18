87af14f22e3ac5d0991d98948694f3b1d0e4b152
kerbage <lkerbage@gmx.net>
2022-12-18 08:56:51 +0100

    WIP: account-settings

--- lib/schema/settings-form-schema.ts
+++ lib/schema/settings-form-schema.ts
@@ -2,6 +2,6 @@ import { object, string } from 'yup'
 
 export const settingsFormSchema = object().shape({
   firstName: string().min(2, 'too short').required('Required field'),
-  lastName: string().email().required('Required field'),
+  lastName: string().min(1, 'too short').required('Required field'),
   phone: string().min(2, 'too short').required('Required field'),
 })
--- pages/user/account-settings.tsx
+++ pages/user/account-settings.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Box, Button, Snackbar, TextField, Typography } from '@mui/material'
+import { Box, Snackbar, TextField, Typography } from '@mui/material'
 import { Formik } from 'formik'
 import { useTranslation } from '../../hooks/use-translation'
 import { SettingType } from '../../components/user/types'
 import { CondensedContainer } from '../../components/condensed-container'
 import { LoadingButton } from '@mui/lab'
 import { settingsFormSchema } from '../../lib/schema/settings-form-schema'
+import { fetchJson } from '../../lib/helpers/fetch-json'
 
 const twFormGroup = 'mb-4'
 
@@ -20,6 +21,35 @@ const AccountSettings: React.FC = () => {
     console.log(values)
     setMessage('SETTINGS_success')
   }
+
+  const handleResetPassword = async (email: string) => {
+    const fetchedResetPassword = await fetchJson<{
+      isOk: boolean
+    }>('/api/reset-password', {
+      method: 'POST',
+      headers: {
+        accept: 'application.json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(email),
+      cache: 'default',
+    })
+    try {
+      if (fetchedResetPassword.isOk) {
+        setIsLoading(false)
+      } else {
+        setOpen(true)
+        setMessage(t('PASSWORD HAS BEEN RESET'))
+        setIsLoading(false)
+      }
+    } catch {
+      setOpen(true)
+      setMessage('LOGIN_server_error')
+      setIsLoading(false)
+    }
+  }
+
+  // todo add pre-filled values
   return (
     <CondensedContainer>
       <Typography variant="h1" className="my-6">
@@ -28,9 +58,9 @@ const AccountSettings: React.FC = () => {
       <Formik
         initialValues={
           {
-            firstName: '',
-            lastName: '',
-            phone: '',
+            firstName: 'firstName from db',
+            lastName: 'lastName from db',
+            phone: 'phone from db',
           } as SettingType
         }
         validationSchema={settingsFormSchema}
@@ -79,8 +109,6 @@ const AccountSettings: React.FC = () => {
                 label={t('GLOBAL_mobile_number')}
                 variant="outlined"
               />
-
-              <Button>Reset Password</Button>
             </Box>
 
             <LoadingButton
@@ -93,6 +121,16 @@ const AccountSettings: React.FC = () => {
           </form>
         )}
       </Formik>
+      <LoadingButton
+        variant="contained"
+        loading={isLoading}
+        onClick={() => {
+          handleResetPassword('lkerbage@gmx.net').then()
+          setIsLoading(true)
+        }}
+      >
+        {t('BUTTON_reset_password')}
+      </LoadingButton>
       <Snackbar
         anchorOrigin={{
           vertical: 'bottom',
