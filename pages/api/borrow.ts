d3263dffb8c349cf07471c225397cf6bd064f4da
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2022-12-25 12:51:18 +0100

    WIP

--- pages/api/borrow.ts
+++ pages/api/borrow.ts
@@ -1,9 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { doc, setDoc } from 'firebase/firestore'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../config/session-config'
+import { db } from '../../config/firebase'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
   const value = req.body
+
+  const { user } = req.session
+
+  if (!user || !user.id) {
+    res.status(500).json({ ok: false })
+    return
+  }
 
   const parsedValue = { ...JSON.parse(value), status: 'pending' }
 
-  res.status(200).json(parsedValue)
+  const messageRef = doc(
+    db,
+    'inbox',
+    user.id,
+    'messages',
+    new Date().getTime().toString()
+  )
+
+  const send = await setDoc(messageRef, parsedValue).then((r) => r)
+  console.log(send)
+
+  res.status(200).json({ ok: true })
 }
+
+export default withIronSessionApiRoute(handler, sessionOptions)
