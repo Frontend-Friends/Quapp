705a11025a00c547db217b9b760a1d480c86bfb7
Philip Park <not@your.concern>
2022-12-27 17:05:15 +0100

    fix: added responsive behaviour to font size

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,10 @@ export const Header: FC<{
   const t = useTranslation()
   return (
     <header>
-      <Typography variant="h1" className="text-violetRed-600">
+      <Typography
+        variant="h1"
+        className="text-violetRed-600 md:text-3xl lg:text-4xl"
+      >
         {title}
       </Typography>
       {imgSrc && (
