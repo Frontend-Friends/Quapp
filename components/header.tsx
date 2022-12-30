b58cc66fbe35ff216c718ce49ad6e190768d92a7
Philip Park <not@your.concern>
2022-12-30 21:46:08 +0100

    fix: removed image component from header so it only represents text in title and lead feat: added option to pass spacing tw classes to title and lead

--- components/header.tsx
+++ components/header.tsx
@@ -1,34 +1,29 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { FC } from 'react'
-import Image from 'next/image'
-import { useTranslation } from '../hooks/use-translation'
+import clsx from 'clsx'
 
 export const Header: FC<{
   title: string
+  titleSpacingClasses?: string
   lead?: string
-  imgSrc?: string
-}> = ({ title, lead, imgSrc }) => {
-  const t = useTranslation()
+  leadSpacingClasses?: string
+}> = ({ title, lead, titleSpacingClasses, leadSpacingClasses }) => {
   return (
     <header>
       <Typography
         variant="h1"
-        className="text-violetRed-600 md:text-3xl lg:text-4xl"
+        className={clsx(
+          'text-violetRed-600 md:text-3xl lg:text-4xl',
+          titleSpacingClasses
+        )}
       >
         {title}
       </Typography>
-      {imgSrc && (
-        <Box className="relative mt-4 w-full pt-[50%]">
-          <Image
-            src={imgSrc}
-            layout="fill"
-            objectFit="cover"
-            alt={t('PRODUCT_image')}
-          />
-        </Box>
-      )}
       {lead && (
-        <Typography variant="subtitle1" mt={2}>
+        <Typography
+          variant="subtitle1"
+          className={clsx('font-medium leading-5', leadSpacingClasses)}
+        >
           {lead}
         </Typography>
       )}
