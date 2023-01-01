44440c186f18dc8bfffc499c947c5f5a61256570
kerbage <lkerbage@gmx.net>
2023-01-01 22:28:52 +0100

    Refined cases when no invitation exists and when a pending invitation exists

--- pages/api/get-invitation-id.ts
+++ pages/api/get-invitation-id.ts
@@ -7,16 +7,25 @@ import { db } from '../../config/firebase'
 async function getInvitationId(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { email } = req.query
+    let invitationId = ''
+
     const invitationCollection = collection(db, 'invitations')
     const q = query(invitationCollection, where('email', '==', email))
     const querySnapshot = getDocs(q)
-    querySnapshot.then((result) =>
-      result.docs.find((document) => {
-        const invitationId = document.id
-        sendResponse(res, { invitationId })
+    querySnapshot
+      .then((result) => {
+        const foundInvitation = result.docs.map((document) => {
+          invitationId = document.id
+        })
+        if (foundInvitation) {
+          sendResponse(res, { invitationId })
+        } else {
+          sendError(res, { message: 'no invitation found' })
+        }
       })
-    )
-    // sendResponse(res, { message: "ok" })
+      .catch((error) => {
+        sendError(res, error)
+      })
   } catch (err) {
     console.error(err)
     sendError(res)
