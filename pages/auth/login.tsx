bfe0b2140069781bf257c10fb3383233e4b9f51b
kerbage <lkerbage@gmx.net>
2023-01-01 12:03:33 +0100

    Rm console.log

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -68,7 +68,6 @@ const Login: FC = () => {
       setIsLoading(true)
 
       try {
-        console.log(values)
         const fetchedLogin = await fetchJson('/api/login', {
           method: 'POST',
           body: JSON.stringify({ ...values }),
