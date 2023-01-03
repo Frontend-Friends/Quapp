45eeb7594a676fbc4d53c2f9bf0d26346bcbf9ef
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2023-01-03 22:00:21 +0100

    Add jest

--- pages/community/dashboard.tsx
+++ pages/community/dashboard.tsx
@@ -1,18 +1,11 @@
-import { FC, useState } from 'react'
-import { CondensedContainer } from '../../components/condensed-container'
-import { useTranslation } from '../../hooks/use-translation'
-import { Fab, Grid, Typography } from '@mui/material'
-import { Header } from '../../components/header'
-import AddIcon from '@mui/icons-material/Add'
-import { InferGetServerSidePropsType } from 'next'
 import { SpaceItemType } from '../../components/products/types'
 import { getDoc } from 'firebase/firestore'
-import SpaceItem from '../../components/spaces/space-item'
 import { withIronSessionSsr } from 'iron-session/next'
 import { sessionOptions } from '../../config/session-config'
 import { getSpaceRef } from '../../lib/helpers/refs/get-space-ref'
-import SpaceForm from './space-form'
 import { fetchUser } from '../../lib/services/fetch-user'
+import { Dashboard } from '../../components/pages/dashboard'
+import { useTranslation } from '../../hooks/use-translation'
 
 export const getServerSideProps = withIronSessionSsr<{
   spaces?: SpaceItemType[]
@@ -43,46 +36,10 @@ export const getServerSideProps = withIronSessionSsr<{
   )
   return { props: { spaces } }
 }, sessionOptions)
-const Dashboard: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
-  spaces,
-}) => {
-  const t = useTranslation()
-  const [open, setOpen] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [message, setMessage] = useState('')
 
-  return (
-    <CondensedContainer className="relative">
-      <Header title={t('SPACES_title')} />
-      {!!spaces?.length ? (
-        <Grid container columns={{ md: 1 }} spacing={{ xs: 4 }} pt={4}>
-          {spaces.map((space) => (
-            <SpaceItem key={space.id} space={space} />
-          ))}
-        </Grid>
-      ) : (
-        <Typography variant="body2">{t('SPACES_no_entries')}</Typography>
-      )}
-      <Fab
-        color="primary"
-        aria-label="add"
-        variant="extended"
-        className="mt-8"
-        onClick={() => setOpen((state) => !state)}
-      >
-        <AddIcon className="mr-2" /> {t('SPACES_add_space')}
-      </Fab>
-      {open && (
-        <SpaceForm
-          setOpen={setOpen}
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
-          setMessage={setMessage}
-          message={message}
-        />
-      )}
-    </CondensedContainer>
-  )
+export const Index = ({ spaces }: { spaces?: SpaceItemType[] }) => {
+  const t = useTranslation()
+  return <Dashboard t={t} spaces={spaces} />
 }
 
-export default Dashboard
+export default Index
