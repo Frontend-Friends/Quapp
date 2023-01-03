e6262e455a0f87bef16675752a97d34372567ec8
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2023-01-03 22:36:25 +0100

    Add getQueryAsNumber to test

---
+++ __tests__/get-query-as-number.test.ts
@@ -0,0 +1,24 @@
+import { getQueryAsNumber } from '../lib/helpers/get-query-as-number'
+
+it('returns 0 from undefined', () => {
+  expect(getQueryAsNumber()).toBe(0)
+})
+
+it('returns 0 from a non digit string', () => {
+  expect(getQueryAsNumber('hello')).toBe(0)
+})
+
+it('returns a number from a digit string', () => {
+  expect(getQueryAsNumber('12')).toBe(12)
+  expect(getQueryAsNumber('300')).toBe(300)
+  expect(getQueryAsNumber('0.300')).toBe(0.3)
+  expect(getQueryAsNumber('0,678900')).toBe(0.6789)
+})
+
+it('returns 0 from a wrong digit string', () => {
+  expect(getQueryAsNumber('12f')).toBe(0)
+  expect(getQueryAsNumber('30,,0')).toBe(0)
+  expect(getQueryAsNumber('0.3.00')).toBe(0)
+  expect(getQueryAsNumber('0,678,900')).toBe(0)
+  expect(getQueryAsNumber('0,678,fda')).toBe(0)
+})
--- lib/helpers/get-query-as-number.ts
+++ lib/helpers/get-query-as-number.ts
@@ -1,2 +1,19 @@
-export const getQueryAsNumber = (query?: string | string[]) =>
-  isNaN(parseInt(query as string)) ? 0 : parseInt(query as string)
+export const getQueryAsNumber = (query?: string | string[]) => {
+  if (typeof query !== 'string') {
+    return 0
+  }
+  // Find any non Digit except comma and decimal
+  const mayHasLetters = query.match(/[^\d\n,.]/g)
+  const mayHasCommas = query.match(/,/g)
+  const mayHasDecimal = query.match(/\./g)
+
+  const toManyCommas = mayHasCommas && mayHasCommas.length > 1
+  const toManyDecimal = mayHasDecimal && mayHasDecimal.length > 1
+
+  if (mayHasLetters || toManyDecimal || toManyCommas) {
+    return 0
+  }
+  const commaToDecimal = query.replace(',', '.')
+
+  return isNaN(parseFloat(commaToDecimal)) ? 0 : parseFloat(commaToDecimal)
+}
