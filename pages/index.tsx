3828f1e436d3ae3a5428f7fe529dacfa3aca71b7
Philip Park <not@your.concern>
2023-01-07 18:12:10 +0100

    feature: homepage header section now fully reponsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,30 +23,35 @@ const Home: FC<{ isLoggedIn: boolean }> = () => {
   const t = useTranslation()
   return (
     <main className="w-full text-blueishGray-600">
-      <section className="relative bg-gradient-to-br from-violetRed-600 via-violetRed-600 to-violetRed-900 pb-10 text-white">
-        <div className="mx-auto max-w-[1280px] p-3 lg:p-8">
+      <section className="relative bg-gradient-to-b from-violetRed-600 via-violetRed-600 to-violetRed-900 pb-10 text-white">
+        <div className="mx-auto grid max-w-[1280px] grid-cols-7 grid-rows-[auto_auto_auto_auto_auto_auto_auto] p-3 lg:p-8 lg:pb-24">
           <LogoSVG
             aria-label={t('SVG_logo')}
-            className="mx-auto block w-1/2 lg:mx-[unset] lg:h-1/2 lg:w-1/5"
+            className="col-span-7 mx-auto block w-1/2 md:h-full md:w-1/5 lg:mb-10"
+            preserveAspectRatio="xMidYMid meet"
           />
-          <h1 className="text-center text-lg font-medium">
+          <h1 className="col-span-7 text-center text-xl font-medium sm:col-start-3 sm:text-left md:text-2xl lg:pl-14 lg:text-3xl">
             {t('HOME_subtitle')}
           </h1>
-          <div className="flex gap-5">
-            <AppartmentSVG className="-ml-24 h-[130px] w-full flex-1" />
-            <p className="m-0 flex-1 font-light">{t('HOME_intro')}</p>
-          </div>
-          <Link underline="none" href="/auth/signup">
+          <AppartmentSVG className="col-span-3 row-span-2 -ml-6 h-[130px] w-full flex-1 sm:col-span-2 sm:-ml-14 sm:scale-125 md:scale-150 lg:scale-[2]" />
+          <p className="col-span-4 row-span-2 m-0 flex-1 text-lg font-light sm:col-span-4 md:text-2xl lg:row-span-1 lg:pl-14">
+            {t('HOME_intro')}
+          </p>
+          <Link
+            underline="none"
+            href="/auth/signup"
+            className="col-span-7 mx-auto mt-5 mb-3 block md:col-start-3 md:mx-[unset] md:mt-0 lg:mb-6 lg:mt-8 lg:pl-14"
+          >
             <Button
               type="submit"
               variant="contained"
               color="secondary"
-              className="mx-auto mt-5 mb-3 block px-12"
+              className="px-12 md:py-3 md:text-xl lg:py-4 lg:text-2xl"
             >
               {t('HOME_signup_free')}
             </Button>
           </Link>
-          <Box className="text-center">
+          <Box className="col-span-7 text-center md:col-start-3 md:text-left lg:pl-14 lg:text-xl">
             <Link
               underline="hover"
               href="/auth/login"
@@ -58,7 +63,7 @@ const Home: FC<{ isLoggedIn: boolean }> = () => {
         </div>
         <WaveWhiteSVG
           preserveAspectRatio="none"
-          className="pointer-events-none absolute bottom-0 w-full touch-none md:h-14 lg:h-20"
+          className="xl:h-30 pointer-events-none absolute bottom-0 w-full touch-none sm:h-12 md:h-14 lg:h-28"
         />
       </section>
       <section className="mx-auto max-w-[1280px] p-3 lg:p-8">
@@ -70,8 +75,8 @@ const Home: FC<{ isLoggedIn: boolean }> = () => {
           <p className="mt-1 text-center">{t('HOME_support_text')}</p>
         </div>
       </section>
-      <section className="relative mx-auto mb-10 pb-24 lg:p-8">
-        <div className="max-w-[1280px] bg-gradient-to-t from-slate-300 to-slate-200">
+      <section className="mb-10 pb-10 lg:p-8">
+        <div className="relative mx-auto max-w-[1280px] bg-gradient-to-t from-slate-300 to-slate-200">
           <WaveWhiteTopSVG
             preserveAspectRatio="none"
             className="absolute top-0 w-full"
