370c178ea85e2de67c129f0f4c4e3655b0a3bc3b
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2023-01-12 22:37:23 +0100

    Debugging

--- lib/scripts/upload-file-to-storage.ts
+++ lib/scripts/upload-file-to-storage.ts
@@ -1,23 +1,22 @@
 import { File } from 'formidable'
 import fs from 'fs'
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
-import { storage } from '../../config/firebase'
 
 export const uploadFileToStorage = async (img?: File | null) => {
   if (!img || !img.originalFilename) {
     return null
   }
-
-  const filePath = `${
-    process.env.NODE_ENV === 'production' ? process.cwd() : ''
-  }${img.filepath}`
+  const filePath = `${process.cwd()}${img.filepath}`
 
   const fileBuffer = await fs.promises.readFile(filePath)
+
   throw Error(fileBuffer.toString())
+  return null
+  /*
 
   const imgRef = ref(storage, `${new Date().getTime()}-${img.originalFilename}`)
 
   await uploadBytes(imgRef, fileBuffer)
 
   return getDownloadURL(imgRef)
+*/
 }
