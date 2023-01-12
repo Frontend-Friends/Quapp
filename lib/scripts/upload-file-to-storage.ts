bac255beabe36edb1dda24909e24b8cb18df5441
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2023-01-12 22:45:28 +0100

    Debugging

--- lib/scripts/upload-file-to-storage.ts
+++ lib/scripts/upload-file-to-storage.ts
@@ -1,5 +1,7 @@
 import { File } from 'formidable'
 import fs from 'fs'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { storage } from '../../config/firebase'
 
 export const uploadFileToStorage = async (img?: File | null) => {
   if (!img || !img.originalFilename) {
@@ -9,14 +11,13 @@ export const uploadFileToStorage = async (img?: File | null) => {
 
   const fileBuffer = await fs.promises.readFile(filePath)
 
-  throw Error(fileBuffer.toString())
-  return null
-  /*
-
   const imgRef = ref(storage, `${new Date().getTime()}-${img.originalFilename}`)
+
+  console.error('to imgRef')
 
   await uploadBytes(imgRef, fileBuffer)
 
+  console.error('after uploadBytes')
+
   return getDownloadURL(imgRef)
-*/
 }
