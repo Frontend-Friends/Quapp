00da2e8754ed780b73be1ea5800bd7d023c9af2d
Philip Park <not@your.concern>
2023-01-16 19:48:45 +0100

    fix: added close button

--- components/products/product-item.tsx
+++ components/products/product-item.tsx
@@ -4,10 +4,11 @@ import {
   Card,
   CardContent,
   CardMedia,
+  IconButton,
   Modal,
   Typography,
 } from '@mui/material'
-import { FC, useState } from 'react'
+import React, { FC, useState } from 'react'
 import { useTranslation } from '../../hooks/use-translation'
 import Link from 'next/link'
 import { ProductType } from './types'
@@ -17,6 +18,7 @@ import { CondensedContainer } from '../condensed-container'
 import { User } from '../user/types'
 import clsx from 'clsx'
 import ImageNotSupportedRoundedIcon from '@mui/icons-material/ImageNotSupportedRounded'
+import CloseIcon from '@mui/icons-material/Close'
 
 export const ProductItem: FC<{
   product: ProductType
@@ -127,6 +129,15 @@ export const ProductItem: FC<{
         aria-describedby="delete-description"
       >
         <CondensedContainer className="absolute m-0 h-full w-full bg-white p-8 drop-shadow-2xl md:top-1/3 md:left-1/2 md:h-[unset] md:w-[600px] md:-translate-x-1/2 md:-translate-y-1/3">
+          <Box className="sticky top-0 z-10 flex h-0 w-full justify-end">
+            <IconButton
+              title={t('BUTTON_close')}
+              className="z-10 -mt-2 h-12 w-12 border border-slate-200 bg-white shadow hover:bg-slate-200"
+              onClick={() => setOpenDeleteModal(false)}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
           <h3 className="m-0 mb-6 pr-12" id="delete-title">{`${t(
             'DELETE_title'
           )} ${product.title}`}</h3>
