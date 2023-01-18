83edcd7ddf2aa9d2421dbfe5e130c106d3170d75
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2023-01-18 08:22:38 +0100

    Add language switch

--- mock/translations/translations-en.ts
+++ mock/translations/translations-en.ts
@@ -1,20 +1,51 @@
 export const translationsEn: Record<string, string> = {
-  GLOBAL_email: 'Email',
+  GLOBAL_email: 'email',
   GLOBAL_password: 'Password',
-  GLOBAL_first_name: 'First name',
-  GLOBAL_last_name: 'Last name',
+  GLOBAL_first_name: 'first_name',
+  GLOBAL_last_name: 'last_name',
+  GLOBAL_user_name: 'nickname',
   GLOBAL_mobile_number: 'Mobile number',
-  GLOBAL_user_name: 'Nickname',
-  GLOBAL_save: 'Save',
+  GLOBAL_save: 'save',
   GLOBAL_cancel: 'Cancel',
   GLOBAL_edit: 'Edit',
   GLOBAL_delete: 'Delete',
+  GLOBAL_members: 'Members',
   GLOBAL_open: 'Open',
   GLOBAL_options: 'Options',
+  GLOBAL_back_to_login: 'Back to login',
+  GLOBAL_hello: 'Hello',
+  GLOBAL_go_to_account_settings: 'Account settings',
+  GLOBAL_go_to_inbox: 'Notifications',
+  GLOBAL_settings: 'Settings',
+  GLOBAL_date: 'Date',
   GLOBAL_back_to_home: 'Back to home page',
+  GLOBAL_status: 'status',
   GLOBAL_invitation_send: 'Send invitation',
   GLOBAL_yes: 'Yes',
   GLOBAL_no: 'No',
+  GLOBAL_required: 'Required field',
+  GLOBAL_too_short: 'Input is too short',
+  GLOBAL_password_min_eight: 'Password must be at least 8 characters long',
+  GLOBAL_invalid_email: 'Email address invalid',
+
+  SPACES_title: 'Your Space',
+  SPACES_no_entries:
+    'Sorry, you are not assigned to any Spaces yet. Just create your own!',
+  SPACES_members: 'Members',
+  SPACES_items: 'items',
+  SPACES_add_space: 'Add Space',
+  SPACES_name: 'space name',
+  SPACES_administrator: 'Administrator',
+  SPACES_edit_ok: 'The Space was successfully modified',
+  SPACES_edit_failed: 'The Space could not be created',
+  SPACES_add_space_text:
+    'Please choose an appropriate name for your own new Space:',
+  SPACES_edit_title: 'Edit Space',
+
+  RESET_reset_password: 'Reset password',
+  RESET_title: 'Reset Password',
+  RESET_text: 'Please enter email address to reset your password',
+  RESET_submit_button: 'Reset password',
 
   HOME_signup_free: 'Register now for free',
   HOME_subtitle: 'The app that connects neighbors',
@@ -22,45 +53,56 @@ export const translationsEn: Record<string, string> = {
     'Need a tool or a helping hand? Quapp allows you to easily contact your neighbors!',
   HOME_support_title: 'Supporting each other.',
   HOME_support_text:
-    'Who doesn`t know it: you just need a drill or someone to help you carry a table for five minutes, but you don`t know your neighbors yet and have no one to ask.',
+    "Who doesn't know it: you just need a drill or someone to help you carry a table for five minutes, but you don't know your neighbors yet and have no one to ask.",
   HOME_spaces_title: 'Spaces.',
   HOME_spaces_text:
     'By creating a Space yourself or joining an existing Space with an invitation, you become part of the community.',
   HOME_offers_title: 'Offerings.',
   HOME_offers_text:
-    'Log in and browse your neighbors offers, or simply post an offer yourself for others.',
+    "Log in and browse your neighbors' offers, or simply post an offer yourself for others.",
   HOME_together_title: 'Being neighbors together.',
   HOME_together_text:
-    'Sharing is caring - in our opinion, every neighborhood should live by this motto! Quapp makes it easy for you to connect with your neighborhood and support each other.',
+    'Sharing is caring - in our opinion, every neighborhood should live by this motto! Quapp allows you to easily connect with your neighborhood and support each other.',
+
+  HOME_is_logged_in:
+    'You are already logged in! To get to your dashboard, just click the following button:',
+  HOME_to_dashboard: 'Go to your dashboard',
 
   LOGIN_title: 'Login',
-  LOGIN_login: 'Login',
+  LOGIN_login: 'Log in',
   LOGIN_forgot_password: 'Forgot password?',
-  LOGIN_has_no_account: 'You don`t have an account yet?',
+  LOGIN_has_no_account: "You don't have an account yet?",
   LOGIN_has_account: 'You already have an account?',
-  LOGIN_password_reset_failed: 'Password reset failed. ',
+  LOGIN_something_went_wrong: 'Something went wrong. Please try again',
+  LOGIN_password_has_been_reset:
+    'Your password has been reset. Please check your email inbox',
+  LOGIN_password_reset_failed: 'The password reset has failed',
 
-  RESET_reset_password: 'Reset password',
-  RESET_title: 'Reset password',
-  RESET_text: 'Enter your email address to reset your password.',
-  RESET_submit_button: 'Reset password',
-
-  LOGOUT_logout: 'Log out',
+  LOGOUT_logout: 'Logout',
 
   SIGNUP_title: 'Registration',
   SIGNUP_signup: 'Register',
+  SIGNUP_something_went_wrong:
+    'Something went wrong during registration. Please try again',
+  SIGNUP_success_page_text:
+    'Thank you for your registration! We have sent you an email with a verification link. Please click it to complete the registration. Your Quapp Team',
 
   PRODUCTS_title: 'Products',
+  PRODUCTS_filter_category_label: 'Filter by category',
+  PRODUCTS_reset_category_filter: 'Reset filter',
   PRODUCTS_no_entries: 'Sorry, there are no entries yet',
   PRODUCT_not_available: 'Not available',
-  PRODUCT_add: 'Create new product',
-  PRODUCT_image: 'Product image',
+  PRODUCT_add: 'Create quote',
+  PRODUCT_image: 'Offer image',
   PRODUCT_edit: 'Edit',
   PRODUCT_delete: 'Delete',
   PRODUCT_updated_info: 'The entry was saved:',
-  PRODUCT_settings: 'Product settings',
-  PRODUCTS_reset_category_filter: 'Reset filter',
-
+  PRODUCT_settings: 'Offer settings',
+  PRODUCT_request_from: 'Request from',
+  PRODUCT_description: 'offer description',
+  PRODUCTS_my_list: 'My offers',
+  PRODUCT_location_at: 'in',
+  PRODUCT_label: 'offer',
   DELETE_cancel_button: 'Cancel',
   DELETE_confirm_button: 'Delete',
   DELETE_title: 'Delete from: ',
@@ -69,77 +111,101 @@ export const translationsEn: Record<string, string> = {
 
   SAVE_PRODUCT_submit: 'Save',
 
-  SETTINGS_updated: 'The settings were saved.',
-  SETTINGS_title: 'Settings',
-  SETTINGS_change_settings: 'Change settings',
-  SETTINGS_change_password: 'Change password',
-  SETTINGS_change_password_description:
-    'To change your password, you need to have it reset by our system. You will receive an email with instructions after doing so.',
   BORROW_TEXTFIELD_label: 'Message',
   BORROW_TEXTFIELD_default_value: 'Write a message',
   BORROW_DATEPICKER_label: 'Borrow date',
   BORROW_button_submit: 'Send request',
+  BORROW_request_text:
+    '{USER} would like to take advantage of the {PRODUCT_TITLE} offer on {DATE}',
+  BORROW_response_text:
+    'You can claim the {PRODUCT_TITLE} offer from {USER} on {DATE} {ACCEPT}',
+  BORROW_response_not: 'not',
+  BORROW_message_title: 'Request for: ',
+  BORROW_response_message_title: 'Response to request for: ',
+  BORROW_response_accept_text: 'You have accepted the request',
+  BORROW_response_declined_text: 'You have declined the request',
+  BORROW_REQUEST_accept: 'Accept',
+  BORROW_REQUEST_decline: 'Decline',
+  BORROW_date_assigned: 'The date is already assigned',
+  BORROW_success_title: 'Your request has been sent',
+  BORROW_success_text: 'Do you want a new request',
+  BORROW_message_response_title: 'Reply to your request',
+  BORROW_new_request: 'Make new request',
 
-  CREATE_PRODUCT_detail: 'Product Detail',
+  REQUEST_product_link: 'Link to quote',
+  REQUEST_decline: 'reject request',
+  REQUEST_accept: 'accept request',
+
+  FORM_submitting_error: 'An error occurred during creation',
+
+  CREATE_PRODUCT_detail: 'Quote detail',
   CREATE_PRODUCT_upload: 'Upload image',
-  CREATE_PRODUCT_description: 'Product Description',
-  CREATE_PRODUCT_title: 'Product Title',
-  CREATE_PRODUCT_text: 'Product Text',
-  CREATE_PRODUCT_submit: 'Create Product',
-  CREATE_PRODUCT_loading: 'Transfer...',
-  CREATE_PRODUCT_page_title: 'Create product',
-  EDIT_PRODUCT_page_title: 'Edit product',
-  CREATE_PRODUCT_is_available: 'Product is available',
+  CREATE_PRODUCT_description: 'Quote Description',
+  CREATE_PRODUCT_title: 'Offer Title',
+  CREATE_PRODUCT_text: 'Offer Subtitle',
+  CREATE_PRODUCT_submit: 'Create Quote',
+  CREATE_PRODUCT_loading: 'transfer...',
+  CREATE_PRODUCT_page_title: 'create quote',
+  CREATE_PRODUCT_category_label: 'select category',
+  CREATE_PRODUCT_add_category: 'Create new category',
+  EDIT_PRODUCT_page_title: 'Edit quote',
+  CREATE_PRODUCT_is_available: 'Offer is available',
   CREATE_PRODUCT_new_category: 'New category',
+  PRODUCT_CREATE_wrong_file_format: 'The image must be either a JPG or PNG',
+  PRODUCT_CREATE_file_size:
+    'The image is too big. It must not be larger than 2MB',
 
   BUTTON_borrow: 'I want to borrow this!',
   BUTTON_contact: 'Contact me',
   BUTTON_info: 'Learn more',
   BUTTON_close: 'Close',
   BUTTON_remove_upload: 'Remove uploaded image',
-  BUTTON_invite_member: 'Invite person',
   BUTTON_reset_password: 'Reset password',
-  BUTTON_save: 'Speichern',
+  BUTTON_invite_member: 'Invite person',
+  BUTTON_save: 'Save',
 
   MENU_invite_member: 'Invite person',
 
-  CHAT_title: 'News',
-
-  SPACES_title: 'Your Spaces',
-  SPACES_no_entries: 'There are no Spaces yet',
-  SPACES_members: 'Members',
-  SPACES_items: 'Entries',
-  SPACES_add_space: 'Add space',
-  SPACES_name: 'Space name',
-  SPACES_administrator: 'Administrator',
-  SPACES_edit_ok: 'The space is successfully edited',
-  SPACES_edit_failed: 'The space could not be edited',
-  SPACES_edit_title: 'Edit space',
+  CHAT_title: 'Chat',
+  CHAT_no_messages: 'There are no chat messages at the moment',
 
   CHAT_button_send: 'Send message',
   CHAT_button_loading: 'will be sent',
-  CHAT_message: 'Message',
-  CHAT_message_error: 'Message must not be empty',
-  CHAT_message_required: 'The field is mandatory',
+  CHAT_message: 'message',
+  CHAT_message_history: 'message history',
+  CHAT_message_error: 'message must not be empty',
+  CHAT_message_required: 'This is a required field',
 
-  INVITATION_server_error: 'An error occurred while sending the invitation',
-  INVITATION_title: 'Invite a person to this space',
+  SETTINGS_title: 'Settings',
+  SETTINGS_change_settings: 'Change settings',
+  SETTINGS_change_password: 'Change password',
+  SETTINGS_change_password_description:
+    'To change your password, you need to have it reset by our system. You will receive an email with instructions after doing so.',
+  SETTINGS_updated: 'The settings have been successfully changed',
 
-  SVG_logo: 'The Quapp logo with link to the home page',
-  SVG_appartment:
-    'A purely decorative illustration showing an apartment building',
-  SVG_collaborator:
-    'A purely decorative illustration showing people working together',
-  SVG_hangout:
-    'A purely decorative illustration showing two people drinking coffee together at a table.',
+  INVITATION_server_error: 'There was an error sending the invitation',
+  INVITATION_title: 'Invite a person to this Space',
+
+  NOTIFICATION_unread_message: 'Unread message',
+  RESPONSE_SERVER_ERROR: 'There was an error while sending',
+
+  HTML_TITLE_general: 'Quapp - The app that connects neighbors',
+  HTML_META_description:
+    'Quapp helps you connect with your neighborhood and support each other with help or loans',
+
+  SVG_logo: 'The Quapp Logo',
+  SVG_apartment:
+    'A simple illustration on which you can see a high-rise building with people from it',
   SVG_windowneighbours:
-    'A purely decorative illustration showing two neighbors talking.',
+    'A simple illustration on which two neighbors are talking about their balconies',
+  SVG_collaborator: 'A simple illustration on which two people can be seen,',
+  SVG_hangout:
+    'A simple illustration of two people drinking coffee and talking at a table',
 
   FOOTER_contact: 'Contact',
   FOOTER_imprint: 'Imprint',
-  FOOTER_privacy: 'Privacy policy',
+  FOOTER_privacy: 'privacy policy',
 
-  RESPONSE_SERVER_ERROR: 'An error occurred while sending the request',
-
-  HTML_TITLE_general: 'Quapp - The app that connects neighbors',
+  INBOX_title: 'notifications',
+  INBOX_no_entries: 'There are no messages yet',
 }
